079706255bd7beb5cf044ff1beccb62ead0c0ab2
MatheuRezende <129076633+MatheuRezende@users.noreply.github.com>
2026-04-13 21:19:48 -0300

    aula 12 final

--- aula12/atividade1.js
+++ aula12/atividade1.js
@@ -30,4 +30,59 @@ switch (cor) {
         console.log("Cor invalida")
         break;
 }
-c
+
+
+
+
+//Peça 2 números ao usuário: um número inicial e um número final de um intervalo. Imprima todos os números entre eles (inclusive) usando um loop while.
+//5 e  50 -> contagem inicia em 5 e termina em 50
+
+let numeroInicial = parseInt(prompt("Digite o número inicial do intervalo: "))
+let numeroFinal = parseInt(prompt("Digite o número final do intervalo: "))
+
+while (numeroInicial<= numeroFinal) {
+    console.log(numeroInicial)
+
+    //atualizar a variável de controle
+    numeroInicial++    
+}
+
+
+
+/* Leia o preço de um produto e a cor da sua etiqueta. De acordo com a cor da etiqueta aplique um desconto sobre o produto. Verde - 10% | Amarelo - 20% | Azul - 30 % | Vermelho 40%*/
+
+let preco = parseFloat(prompt("Digite o preço do produto: "))
+let corEtiqueta = prompt("Digite a cor da etiqueta: ").toLowerCase() // Transforma a string para minúscula
+let desconto= 0
+
+//verde, amarelo, azul, vermelho
+
+switch (corEtiqueta) {
+    case 'verde':
+        //10% de desconto
+        //preco = preco - (preco * 0.1)
+        preco = preco * 0.9 // 90% do preço original
+        desconto = 10
+        break;
+    case 'amarelo':
+        //20% de desconto
+        //preco = preco - (preco * 0.2)
+        preco = preco * 0.8 // 80% do preço original
+        desconto = 20
+        break;
+    case 'azul':
+        //30% de desconto
+        preco = preco * 0.7 // 70% do preço original
+        desconto = 30
+        break;
+    case 'vermelho':
+        //40% de desconto
+        preco = preco * 0.6 // 60% do preço original
+        desconto = 40
+        break;
+    default:
+        desconto = 0
+        break;
+}
+
+console.log(`O preço final do produto é R$ ${preco.toFixed(2)} com um desconto de ${desconto}%`)
--- aula12/while.js
+++ aula12/while.js
@@ -22,14 +22,52 @@
 
 // //Peça 2 números ao usuário: um número inicial e um número final de um intervalo. Imprima todos os números entre eles
 // (inclusive) usando um loop while.
-// //5 e 50 -> contagem inicia em 5 e termina em 50
+// // //5 e 50 -> contagem inicia em 5 e termina em 50
 
-let num1 = Number(prompt("Digite o primeiro numero"))
-let num2 = Number(prompt("Digite o segundo numero"))
+// let num1 = Number(prompt("Digite o primeiro numero"))
+// let num2 = Number(prompt("Digite o segundo numero"))
 
-while (num1 <= num2) {
-    console.log(num1)
+// while (num1 <= num2) {
+//     console.log(num1)
 
-    // atualizando da variaçao de controle  
-    num1++      
-}
+//     // atualizando da variaçao de controle  
+//     num1++      
+// }
+
+
+
+
+
+
+// //exibir todos os numeros de 1 a 10 e mostrar a soma
+// desses números
+// let contador = 1 //variavel de controle do laço
+// let soma = 0 //acumulador para a soma dos números
+
+// while (contador <= 10) {
+// console.log(contador) //exibe o número atual
+// soma += contador //adiciona o número atual à soma
+// contador++ //incrementa o contador para o próximo
+// //número, atualizando a variável de controle
+
+// 9
+
+// 10
+
+// 11
+
+// console.log("A soma dos números de 1 a 10 é: " + soma)
+
+
+
+
+// //Faça um programa que peça um número positivo ao
+// usuário e imprima todos os números pares de 0 até
+// esse número, inclusive.
+// //faça a soma de todos os números pares encontrados e
+// todos os números impares ( duas somas separadas)
+
+// //para verificar a condição de paridade usaremos o
+// operador "%" (resto da divisao)
+
+// Pedir um número ao usuário
